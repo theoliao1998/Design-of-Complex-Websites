de593dc25880a887c3d0c58d80e96a3c048fd98d
unknown <theoliao1998@gmail.com>
2019-11-19 14:28:18 -0500

    update

--- 8 jQuery/js/challenge5.js
+++ 8 jQuery/js/challenge5.js
@@ -1,6 +1,19 @@
-$(".preview").mouseover(function(){
+function defaultContent(){
+    console.log("leave");
+    $("#image").css({"background":"url('')",'background-color': '#8e68ff'});
+    $("#image").text("Hover over an image below.");
+}
+
+function newContent(){
     $("#image").css({"background":"url("+$(this).attr("src")+")","background-size":"100% 100%"});
     $("#image").text($(this).attr("alt"));
-})
+}
+
+$(".preview").mouseover(newContent);
+$(".preview").focus(newContent);
+$(".preview").mouseleave(defaultContent);
+$(".preview").blur(defaultContent);
+
+
 
 
