224eed9cdf5c87c6c697e40c583e01e82f1feb20
unknown <theoliao1998@gmail.com>
2019-11-19 14:47:24 -0500

    update

--- 8 jQuery/js/challenge3.js
+++ 8 jQuery/js/challenge3.js
@@ -1,10 +1,15 @@
-$("#ch3form").submit(function(){
+$("#ch3form").submit(function(e){
+    let msg = ''
     if (!$('input[name="fruit"]:checked').length){
-        alert("You must pick a fruit!");
-        return false;
-    } else if(!$('input[name="standing"]:checked').length){
-        alert("You must pick a standing!");
-        return false;
+        e.preventDefault();
+        msg += "You must pick a fruit!\n";
+    }
+    if(!$('input[name="standing"]:checked').length){
+        e.preventDefault();
+        msg += "You must pick a standing!";
+    }
+    if(msg){
+        alert(msg);
     }
   });
 
--- 8 jQuery/js/challenge4.js
+++ 8 jQuery/js/challenge4.js
@@ -1,9 +1,11 @@
-$("#ch4form").submit(function(){
+$("#ch4form").submit(function(e){
     let name_filled = $("#fullname").val()!="";
     let addr_filled = $("#streetaddr").val()!="";
+    if(!(name_filled && addr_filled)){
+        e.preventDefault();
+    }
     $("#nameerrormsg").toggle(!name_filled);
     $("#addrerrormsg").toggle(!addr_filled);
-    return name_filled && addr_filled;
   });
 
 
